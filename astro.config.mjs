4082ad3f5991f775f75570718e3030c4ed5b8104
lpernelle-woosmap <lpernelle@woosmap.com>
2025-12-08 20:01:28 +0100

    fix: Dynamically resolve asset paths and base URL for environment-specific deployments.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lpernelle-woosmap.github.io',
-    base: '/pattedoie',
+    base: process.env.NODE_ENV === 'production' ? '/pattedoie' : '/',
     vite: {
         server: {
             allowedHosts: ['pattedoie-showcase.loca.lt'],
