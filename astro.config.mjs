7ab41310952eb16432c3820991f6c58885bd48db
lpernelle-woosmap <lpernelle@woosmap.com>
2026-03-14 17:15:54 +0100

    fix: update site URL to lagaille.fr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://pattedoie.pernelle.eu',
+    site: 'https://lagaille.fr',
     base: '/',
-    vite: {
-        server: {
-            allowedHosts: ['https://pattedoie.pernelle.eu'],
-        },
-    },
 });
